bc175c8ed9d2a6d2e3229a77315653d3c5c7adc6
KevinMendes <mendes.Kevin35@gmail.com>
2020-06-19 10:49:39 +0200

    adding mathtype

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -30,6 +30,8 @@ import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
 
+import MathType from '@wiris/mathtype-ckeditor5';
+
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
@@ -56,7 +58,8 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+	MathType
 ];
 
 // Editor configuration.
@@ -65,6 +68,7 @@ ClassicEditor.defaultConfig = {
 		items: [
 			'heading',
 			'|',
+			'mathtype',
 			'bold',
 			'italic',
 			'link',
@@ -98,5 +102,5 @@ ClassicEditor.defaultConfig = {
 		]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
+	language: 'fr'
 };
